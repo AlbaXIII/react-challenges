b917eea76355a9309a1416c1539e8705247369f4
AlbaXIII <alex.jbaker20@googlemail.com>
2025-03-08 16:18:23 +0000

    map functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import css from './App.module.css';
 // import NavBarSimple from './components/NavBarSimple.js';
 import Sidebar from './components/Sidebar.js';
 import NavBarForm from "./components/NavBarForm.js";
+import Content from "./components/Content.js";
 
 function App() {
   return (
     <div className={css.App}>
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
