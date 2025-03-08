c6a5f334fb1d47c1fbd54980b9d597fc98cc8be6
AlbaXIII <alex.jbaker20@googlemail.com>
2025-03-08 15:57:03 +0000

    handling events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import css from './App.module.css';
+// import NavBarSimple from './components/NavBarSimple.js';
 import Sidebar from './components/Sidebar.js';
+import NavBarForm from "./components/NavBarForm.js";
 
 function App() {
   return (
     <div className={css.App}>
+      <NavBarForm />
       <Sidebar />
     </div>
   );
