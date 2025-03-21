7dfe1593bb14bfe688d43d3cc8ae87c1c82a70ba
AlbaXIII <alex.jbaker20@googlemail.com>
2025-03-21 16:11:34 +0000

    hooks & network requests

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,22 @@
-import css from './App.module.css';
-// import NavBarSimple from './components/NavBarSimple.js';
-import Sidebar from './components/Sidebar.js';
-import NavBarForm from "./components/NavBarForm.js";
-import Content from "./components/Content.js";
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
     <div className={css.App}>
+      {/* Add your components here */}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
